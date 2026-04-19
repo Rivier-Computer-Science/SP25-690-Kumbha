c3cc07a4a4ade62e27f27383fb3349f03f9ca623
dkumbha <dkumbha@rivier.edu>
2026-04-19 20:14:20 +0530

    Refactor ViTSelective initialization parameters

--- models/vit.py
+++ models/vit.py
@@ -3,9 +3,10 @@ import torch.nn as nn
 import timm
 
 class ViTSelective(nn.Module):
-    def __init__(self, model_name='vit_small_patch16_224', num_classes=10, pretrained=True):
+    def __init__(self, num_classes=10, img_size=224):
         super(ViTSelective, self).__init__()
-        self.vit = timm.create_model(model_name, pretrained=pretrained, num_classes=num_classes, img_size=224)
+        self.vit = timm.create_model('vit_small_patch16_224', pretrained=True, 
+                                     num_classes=num_classes, img_size=img_size)
         self.confidence = nn.Linear(self.vit.num_features, 1)
 
     def forward(self, x):
